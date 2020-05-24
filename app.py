b0ae1b7dfaa89733f84c576164413cc1300a3e32
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-24 10:48:24 +0300

    Убрал ненужное

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from loader import bot, storage
 from utils.set_bot_commands import set_default_commands
 
 
@@ -13,13 +12,8 @@ async def on_startup(dp):
     await set_default_commands(dp)
 
 
-async def on_shutdown(dp):
-    await bot.close()
-    await storage.close()
-
-
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp, on_startup=on_startup)
