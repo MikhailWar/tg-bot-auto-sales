3f0ca18ca501d02473a764cc027e65da60dcbf26
Maiss-python <67838942+Maiss-python@users.noreply.github.com>
2020-12-23 20:05:25 +0600

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,7 +7,5 @@ from loader import dp, db
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await db.add_user(user_id=message.from_user.id, full_name=message.from_user.full_name)
-    print("Мы добавили")
-    await message.answer(f'Привет, {message.from_user.full_name}!',
-                         reply_markup=my_profile)
+    await message.answer(f'Привет, {message.from_user.full_name}!')
 
